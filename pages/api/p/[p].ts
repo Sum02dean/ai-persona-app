892907ee9d8e988eac3bc8de3ec704c7a1ae50bc
James Twisleton <james@consultwiz.biz>
2023-12-19 21:36:15 +0000

    querying both models with additional prompt

--- pages/api/p/[p].ts
+++ pages/api/p/[p].ts
@@ -1,42 +1,56 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { connectToDatabase, addToCollection } from '../../../utils/mongo';
 import { prettifyUrlProvidedName } from '@/utils/strings';
+import createPersonaWithDalle from '@/utils/dall-e';
+import createPersonaWithOpenjourney from '@/utils/openjourney';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    const { p } = req.query;
-    let prettifiedName: string;
+    const { p, prompt, model } = req.query;
 
+    let prettifiedName: string;
     if (typeof p === 'string') {
         prettifiedName = prettifyUrlProvidedName(p);
     } else {
-        // Handle the case where urlProvidedName is not a string
-        // You might want to throw an error, set a default value, or handle it in another way
+        // Handle the case where provided name p is not a string
         console.error('p is not a string');
-        prettifiedName = "";
+        return res.status(400).json({ error: `Unable to parse provided name ${p}` });
     }
 
+    // Check mongo for the provided name
+    let data = null;
     try {
-        // Connect to the database
         const { db } = await connectToDatabase();
 
-        // Fetch data from 'personas' collection
-        const data = await db.collection('personas').find({ name: prettifiedName }).toArray();
-
-        if (data.length > 0) {
-            console.log("Found documents:", data);
-        } else {
-            console.log(`No documents found with the name ${prettifiedName}`);
-        }
-
-        // // Add the new user to the "users" collection
-        // await addToCollection(db, 'personas', exampleNewUser);
-
-        // Send the response with the fetched data
-        res.status(200).json(data);
-
-
+        // // Fetch data from 'personas' collection
+        data = await db.collection('personas').find({ name: prettifiedName }).toArray();
     } catch (e) {
         console.error(e);
-        res.status(500).json({ error: 'Unable to connect to the database or fetch data' });
     }
+    if (data != null && data.length > 0) {
+        console.log("Found documents:", data);
+        return res.status(200).json(data);
+    }
+
+    console.log(`No documents found with the name ${prettifiedName}`);
+
+    // call either model and return
+    let imageUrl: string;
+    switch (model) {
+        case "openjourney":
+            // TODO: put prompt in post request
+            imageUrl = await createPersonaWithOpenjourney(prettifiedName, prompt);
+            res.redirect(imageUrl);
+            // return res.status(200).json({"model":model, "image_url":imageUrl});
+        case "dall-e":
+            imageUrl = await createPersonaWithDalle(prettifiedName, prompt);
+            res.redirect(imageUrl);
+            // return res.status(200).json({"model":model, "image_url":imageUrl});
+        default:
+            throw new Error(`Unsupported model: ${model}`);
+    }
+    
+
+    // // Add the new user to the "users" collection
+    // await addToCollection(db, 'personas', exampleNewUser)
+
 }
